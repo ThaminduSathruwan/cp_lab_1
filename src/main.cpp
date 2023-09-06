a93bf9e503d2264de9a6804161a8b20d6d15e4c5
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2023-09-06 13:24:35 +0530

    Added additional commandline arguments to get 'm_Member', 'm_Insert', and 'm_Delete'

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 {
     int n, m, type;
 
-    if (argc != 3)
+    if (argc != 7)
     {
         print_usage();
         return 1;
@@ -21,6 +21,25 @@ int main(int argc, char **argv)
     type = atoi(argv[3]);
 
     if (n <= 0 || m <= 0 || (type != 1 && type != 2 && type != 3))
+    {
+        print_usage();
+        return 1;
+    }
+
+    double mMember, mInsert, mDelete;
+    try
+    {
+        mMember = std::stod(argv[4]);
+        mInsert = std::stod(argv[5]);
+        mDelete = std::stod(argv[6]);
+    }
+    catch (std::exception &e)
+    {
+        print_usage();
+        return 1;
+    }
+
+    if (mMember < 0 || mInsert < 0 || mDelete < 0 || (mMember + mInsert + mDelete) != 1)
     {
         print_usage();
         return 1;
@@ -61,8 +80,11 @@ void *thread_func(void *arg)
 
 void print_usage()
 {
-    std::cerr << "Usage: ./linked_list <n> <m> <type>" << std::endl
-              << "\tn:\tnumber of initial elements in the linked list" << std::endl
-              << "\tm:\tnumber of operations per thread" << std::endl
-              << "\ttype:\t1 for serial, 2 for mutex, 3 for rwlock" << std::endl;
+    std::cerr << "Usage: ./linked_list <n> <m> <type> <m_Member> <m_Insert> <m_Delete>" << std::endl
+              << "\tn:\t\tnumber of initial elements in the linked list" << std::endl
+              << "\tm:\t\tnumber of operations per thread" << std::endl
+              << "\ttype:\t\t1 for serial, 2 for mutex, 3 for rwlock" << std::endl
+              << "\tm_Member:\tfraction of m operations that are member operations" << std::endl
+              << "\tm_Insert:\tfraction of m operations that are insert operations" << std::endl
+              << "\tm_Delete:\tfraction of m operations that are delete operations" << std::endl;
 }
