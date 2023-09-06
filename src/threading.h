9ffe9ec080216ebc64c0cc81846f681af3e2682e
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2023-09-06 19:01:31 +0530

    Updated 'ThreadArgs' structure

--- src/threading.h
+++ src/threading.h
@@ -3,18 +3,21 @@
 
 #include <iostream>
 #include <unordered_set>
+#include <vector>
 #include <pthread.h>
 #include "linked_list.h"
 
 typedef struct ThreadArgs
 {
     LinkedList *list;
-    std::unordered_set<int> *insertVals;
-    std::unordered_set<int> *deleteVals;
+    std::vector<int> *insertVals;
+    std::vector<int> *deleteVals;
     int m;
     double mMember;
     double mInsert;
     double mDelete;
+    int threadCnt;
+    pthread_t *tid;
 } ThreadArgs;
 
 void *thread_func(void *arg);
